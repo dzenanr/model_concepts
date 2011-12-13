cac5485e111231edb59033207bdd1163b23ebc25
Dzenan Ridjanovic <dzenanr@gmail.com>
2011-12-12 22:01:38 -0500

    moving boxes

--- Board.dart
+++ Board.dart
@@ -1,11 +1,15 @@
 class Board {
   
-  int x = 0;
-  int y = 0;
+  final int INTERVAL = 10; // ms
+  
   int width;
   int height;
   
   CanvasRenderingContext2D context;
+  
+  Box box1;
+  Box box2;
+  Box box3;
   
   Board(CanvasElement canvas) {
     context = canvas.getContext("2d");
@@ -13,18 +17,33 @@ class Board {
     height = canvas.height;
     border();
     init();
+    // Redraw every INTERVAL ms.
+    document.window.setInterval(redraw, INTERVAL);
   }
   
   void init() {
-    new Box(this, 20, 20, 40, 60);
-    new Box(this, 120, 40, 60, 40);
+    box1 = new Box(this, 20, 20, 80, 40);
+    box2 = new Box(this, 120, 220, 40, 80);
+    box3 = new Box(this, 60, 80, 80, 40);
   }
   
   void border() {
     context.beginPath();
-    context.rect(x, y, width, height);
+    context.rect(0, 0, width, height);
     context.closePath();
     context.stroke();
+  }
+  
+  void clear() {
+    context.clearRect(0, 0, width, height);
+    border();
+  } 
+  
+  void redraw() {
+    clear(); 
+    box1.draw();
+    box2.draw();
+    box3.draw();
   }
 
 }
--- Box.dart
+++ Box.dart
@@ -9,6 +9,7 @@ class Box {
   
   Box(this.board, this.x, this.y, this.width, this.height) {
     draw();
+    document.on.mouseMove.add(onMouseMove);
   }
   
   void draw() {
@@ -16,6 +17,23 @@ class Box {
     board.context.rect(x, y, width, height);
     board.context.closePath();
     board.context.stroke();
+  }
+  
+  bool isPointInside(int px, int py) {
+    if ((px > x && px < x + width) && (py > y && py < y + height))
+      return true;
+  }
+  
+  // Change a position of the box with mouse mouvements.  
+  void onMouseMove(MouseEvent event) {
+    if (isPointInside(event.offsetX, event.offsetY)) {
+      x = event.offsetX - width / 2;
+      if (x < 0) x = 1;
+      if (x > board.width - width) x = board.width - width - 1;
+      y = event.offsetY - height / 2;
+      if (y < 0) y = 1;
+      if (y > board.height - height) y = board.height - height - 1;
+    }
   }
 
 }
