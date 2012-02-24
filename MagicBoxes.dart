00ef603680eeee2ac8cfb94ad9f5b8e16c3ace7d
Dzenan Ridjanovic <dzenanr@gmail.com>
2012-02-24 11:14:13 +0100

    script changes in s03a

--- MagicBoxes.dart
+++ MagicBoxes.dart
@@ -2,6 +2,8 @@
 #source('Box.dart');
 #source('Board.dart');
 
+// branch s03a
+
 // See the style guide: http://www.dartlang.org/articles/style-guide/.
 // See the canvas tutorial: http://dev.opera.com/articles/view/html5-canvas-painting/
 
