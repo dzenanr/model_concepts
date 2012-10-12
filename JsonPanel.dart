946f8867b85813d51803434817ff8d97936d2bf0
Dzenan Ridjanovic <dzenanr@gmail.com>
2012-10-12 17:09:37 -0400

    pretty json

--- JsonPanel.dart
+++ JsonPanel.dart
@@ -1,14 +1,19 @@
 class JsonPanel {
-  
+
   final Board board;
-  
+
   TextAreaElement modelJsonTextArea;
   ButtonElement fromModelToJsonButton;
   ButtonElement fromJsonToModelButton;
-  ButtonElement clearButton;
-  
+  ButtonElement clearJsonButton;
+
+  ParagraphElement prettyParagraph;
+  ButtonElement prettyJsonButton;
+  ButtonElement clearPrettyButton;
+
   JsonPanel(this.board) {
     modelJsonTextArea = document.query('#modelJson');
+
     fromModelToJsonButton = document.query('#fromModelToJson');
     fromModelToJsonButton.on.click.add((MouseEvent e) {
       modelJsonTextArea.value =  board.toJson();
@@ -18,14 +23,52 @@ class JsonPanel {
     fromJsonToModelButton.on.click.add((MouseEvent e) {
       board.fromJson(modelJsonTextArea.value);
     });
-    clearButton = document.query('#clearJson');
-    clearButton.on.click.add((MouseEvent e) {
-      clear();
+    clearJsonButton = document.query('#clearJson');
+    clearJsonButton.on.click.add((MouseEvent e) {
+      modelJsonTextArea.value = '';
+    });
+
+    prettyParagraph = document.query('#fromJsonToPretty');
+    prettyParagraph.contentEditable = "true";
+
+    prettyJsonButton = document.query('#prettyJson');
+    prettyJsonButton.on.click.add((MouseEvent e) {
+      prettyParagraph.innerHTML = prettyJson(modelJsonTextArea.value);
+    });
+    clearPrettyButton = document.query('#clearPretty');
+    clearPrettyButton.on.click.add((MouseEvent e) {
+      prettyParagraph.innerHTML = '';
     });
   }
-  
-  void clear() {
-    modelJsonTextArea.value = '';
+
+  // based on http://ketanjetty.com/coldfusion/javascript/format-json/
+  prettyJson(String json) {
+    var pretty = '';
+    var position = 0;
+    var indent = '&nbsp;&nbsp;&nbsp;&nbsp;';
+    var newLine = '<br />';
+    var char = '';
+    for (var i = 0; i < json.length; i++) {
+      char = json.substring(i, i+1);
+      if (char == '}' || char == ']') {
+        pretty = '${pretty}${newLine}';
+        position = position - 1;
+        for (var j = 0; j < position; j++) {
+          pretty = '${pretty}${indent}';
+        }
+      }
+      pretty = '${pretty}${char}';
+      if (char == '{' || char == '[' || char == ',') {
+        pretty = '${pretty}${newLine}';
+        if (char == '{' || char == '[') {
+          position = position + 1;
+        }
+        for (var k = 0; k < position; k++) {
+          pretty = '${pretty}${indent}';
+        }
+      }
+    }
+    return pretty;
   }
 
 }
--- MenuBar.dart
+++ MenuBar.dart
@@ -1,23 +1,23 @@
 class MenuBar {
-  
+
   final Board board;
-  
+
   // Model
   InputElement modelNameInput;
   ButtonElement openModelButton;
   ButtonElement saveModelButton;
   ButtonElement closeModelButton;
-  
+
   // Edit
   ButtonElement deleteSelectionButton;
-  
+
   // Select
   ButtonElement selectAllButton;
   ButtonElement selectBoxesButton;
   ButtonElement selectLinesButton;
   ButtonElement selectBoxLinesButton;
   ButtonElement selectLinesBetweenBoxesButton;
-  
+
   // View
   ButtonElement increaseSelectionHeightButton;
   ButtonElement decreaseSelectionHeightButton;
@@ -28,25 +28,25 @@ class MenuBar {
   ButtonElement hideSelectionButton;
   ButtonElement hideNonSelectionButton;
   ButtonElement showHiddenButton;
-  
+
   // Create
   ButtonElement createBoxesInDiagonalButton;
   ButtonElement createBoxesAsTilesButton;
-  
-  MenuBar(this.board) { 
+
+  MenuBar(this.board) {
     modelNameInput = document.query('#model-name');
     openModelButton = document.query('#open-model');
     saveModelButton = document.query('#save-model');
     closeModelButton = document.query('#close-model');
-    
+
     deleteSelectionButton = document.query('#delete-selection');
-    
+
     selectAllButton = document.query('#select-all');
     selectBoxesButton = document.query('#select-boxes');
     selectLinesButton = document.query('#select-lines');
     selectBoxLinesButton = document.query('#select-box-lines');
     selectLinesBetweenBoxesButton = document.query('#select-lines-between-boxes');
-    
+
     increaseSelectionHeightButton = document.query('#increase-selection-height');
     decreaseSelectionHeightButton = document.query('#decrease-selection-height');
     increaseSelectionWidthButton = document.query('#increase-selection-width');
@@ -56,10 +56,10 @@ class MenuBar {
     hideSelectionButton = document.query('#hide-selection');
     hideNonSelectionButton = document.query('#hide-non-selection');
     showHiddenButton = document.query('#show-hidden');
-    
+
     createBoxesInDiagonalButton = document.query('#create-boxes-in-diagonal');
     createBoxesAsTilesButton = document.query('#create-boxes-as-tiles');
-    
+
     // Menu bar events.
     openModelButton.on.click.add((MouseEvent e) {
       String modelName = modelNameInput.value.trim();
@@ -70,14 +70,14 @@ class MenuBar {
       board.saveModel(modelName);
     });
     closeModelButton.on.click.add((MouseEvent e) {
-      modelNameInput.value = '';
+      //modelNameInput.value = '';
       board.closeModel();
     });
-    
+
     deleteSelectionButton.on.click.add((MouseEvent e) {
       board.deleteSelection();
     });
-    
+
     selectAllButton.on.click.add((MouseEvent e) {
       board.select();
     });
@@ -93,7 +93,7 @@ class MenuBar {
     selectLinesBetweenBoxesButton.on.click.add((MouseEvent e) {
       board.selectLinesBetweenBoxes();
     });
-    
+
     increaseSelectionHeightButton.on.click.add((MouseEvent e) {
       board.increaseHeightOfSelectedBoxes();
     });
@@ -121,7 +121,7 @@ class MenuBar {
     showHiddenButton.on.click.add((MouseEvent e) {
       board.showHidden();
     });
-    
+
     createBoxesInDiagonalButton.on.click.add((MouseEvent e) {
       board.createBoxesInDiagonal();
     });
